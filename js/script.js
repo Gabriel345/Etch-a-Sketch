dc6646a719db740f53ea1f02eeb6e6553f683b07
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-07-24 11:04:30 -0300

    Lógica da criação dos pixel concluida
    Leves ajustes para fazer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-const container= document.getElementById('container');
+const container = document.getElementById('container');
 
-for (let i = 1; i <= 16; i++){
+for (let i = 1; i <= 16; i++) {
     const novaDiv = document.createElement('div');
-    novaDiv.textContent=`essa é a div nº${i}`;
     novaDiv.classList.add('nova-div');
     container.appendChild(novaDiv);
+
+    novaDiv.addEventListener('mousemove', (e) => {
+        const { offsetX, offsetY } = e;
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel-trail');
+        pixel.style.left = `${offsetX - 10}px`;
+        pixel.style.top = `${offsetY - 10}px`; 
+
+        novaDiv.appendChild(pixel);
+    });
 }
